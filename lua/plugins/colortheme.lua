bd1b168d8660de9fc852dbe1081a819e85dee3a3
Lucas Aguiar <laguiar@esapiens.com.br>
2025-03-04 10:39:10 -0500

    monokai theme

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -2,13 +2,9 @@
 --
 return {
     {
-        "Rigellute/shades-of-purple.vim",
+        "tanvirtin/monokai.nvim",
         lazy = false, -- Garante que o tema seja carregado na inicialização
-        priority = 1000, -- Carregar antes dos outros plugins
-        config = function()
-            vim.opt.termguicolors = true
-            vim.cmd([[colorscheme shades_of_purple]])
-        end
+        priority = 1000 -- Carregar antes dos outros plugins
     }
 }
 
